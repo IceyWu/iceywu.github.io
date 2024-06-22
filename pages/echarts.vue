787354a0bff74d1cf2ab62c604b037778e071048
IceyWu <3128006406@qq.com>
2024-06-22 11:32:57 +0800

    fix: test

--- pages/echarts.vue
+++ pages/echarts.vue
@@ -1,307 +1,308 @@
 <script setup lang="ts">
-import { use } from 'echarts/core'
-import { GraphChart, PieChart } from 'echarts/charts'
+// import { use } from 'echarts/core'
+// import { GraphChart, PieChart } from 'echarts/charts'
 
-import { CanvasRenderer } from 'echarts/renderers'
-import {
-  LegendComponent,
-  TitleComponent,
-  TooltipComponent,
-} from 'echarts/components'
-import VChart, { THEME_KEY } from 'vue-echarts'
-import type { EChartsOption } from 'echarts'
+// import { CanvasRenderer } from 'echarts/renderers'
+// import {
+//   LegendComponent,
+//   TitleComponent,
+//   TooltipComponent,
+// } from 'echarts/components'
+// import VChart, { THEME_KEY } from 'vue-echarts'
+// import type { EChartsOption } from 'echarts'
 
-const isDark = useDark()
+// const isDark = useDark()
 
-use([
-  CanvasRenderer,
-  PieChart,
-  GraphChart,
-  TitleComponent,
-  TooltipComponent,
-  LegendComponent,
-])
+// use([
+//   CanvasRenderer,
+//   PieChart,
+//   GraphChart,
+//   TitleComponent,
+//   TooltipComponent,
+//   LegendComponent,
+// ])
 
-provide(
-  THEME_KEY,
-  computed(() => (isDark.value ? 'dark' : '')),
-)
-const tempData = ref({
-  // 方向
-  direction: [
-    {
-      name: '前端',
-      val: 'web',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-html.svg',
-    },
-    {
-      name: '后端',
-      val: 'backend',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-apib.svg',
-    },
-    {
-      name: 'IOS',
-      val: 'ios',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-applescript.svg',
-    },
-    {
-      name: 'APP',
-      val: 'app',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-appsemble.svg',
-    },
-  ],
-  // 语言
-  language: [
-    {
-      name: 'JavaScript',
-      target: ['Vue', 'Nuxt', '前端', '后端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-js-official.svg',
-    },
-    {
-      name: 'TypeScript',
-      target: ['Vue', 'Nuxt', '前端', '后端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-tsconfig.svg',
-    },
-    {
-      name: 'Python',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-python.svg',
-    },
-    {
-      name: 'CSS',
-      target: ['Vue', 'Nuxt', '前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-css.svg',
-    },
-    {
-      name: 'HTML',
-      target: ['Vue', 'Nuxt', '前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-html.svg',
-    },
-    {
-      name: 'C',
-      target: ['APP'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-c.svg',
-    },
-    {
-      name: 'Lua',
-      icon: 'https://api.iconify.design/vscode-icons:file-type-lua.svg',
-    },
-    {
-      name: 'C++',
-      target: ['APP'],
-      icon: '',
-    },
-    {
-      name: 'Swift',
-      target: ['IOS', 'APP'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-swift.svg',
-    },
-    {
-      name: 'Objective-C',
-      target: ['IOS', 'APP'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-objectivec.svg',
-    },
-    {
-      name: 'PHP',
-      target: ['后端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-php.svg',
-    },
-  ],
-  // 框架
-  frame: [
-    {
-      name: 'Vue',
-      target: ['前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-vue.svg',
-    },
-    {
-      name: 'Nuxt',
-      target: ['前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-nuxt.svg',
-    },
-    {
-      name: 'Electron',
-      target: ['前端', 'APP'],
-      icon: 'https://api.iconify.design/vscode-icons:folder-type-electron.svg',
-    },
-    {
-      name: 'Tauri',
-      target: ['前端', 'APP'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-tauri.svg',
-    },
-    {
-      name: 'Vite',
-      target: ['前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-vite.svg',
-    },
-    {
-      name: 'Unocss',
-      target: ['前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-unocss.svg',
-    },
-    {
-      name: 'Webpack',
-      target: ['前端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-webpack.svg',
-    },
-    {
-      name: 'Nestjs',
-      target: ['后端'],
-      icon: 'https://api.iconify.design/vscode-icons:file-type-nestjs.svg',
-    },
-  ],
-})
+// provide(
+//   THEME_KEY,
+//   computed(() => (isDark.value ? 'dark' : '')),
+// )
+// const tempData = ref({
+//   // 方向
+//   direction: [
+//     {
+//       name: '前端',
+//       val: 'web',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-html.svg',
+//     },
+//     {
+//       name: '后端',
+//       val: 'backend',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-apib.svg',
+//     },
+//     {
+//       name: 'IOS',
+//       val: 'ios',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-applescript.svg',
+//     },
+//     {
+//       name: 'APP',
+//       val: 'app',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-appsemble.svg',
+//     },
+//   ],
+//   // 语言
+//   language: [
+//     {
+//       name: 'JavaScript',
+//       target: ['Vue', 'Nuxt', '前端', '后端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-js-official.svg',
+//     },
+//     {
+//       name: 'TypeScript',
+//       target: ['Vue', 'Nuxt', '前端', '后端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-tsconfig.svg',
+//     },
+//     {
+//       name: 'Python',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-python.svg',
+//     },
+//     {
+//       name: 'CSS',
+//       target: ['Vue', 'Nuxt', '前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-css.svg',
+//     },
+//     {
+//       name: 'HTML',
+//       target: ['Vue', 'Nuxt', '前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-html.svg',
+//     },
+//     {
+//       name: 'C',
+//       target: ['APP'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-c.svg',
+//     },
+//     {
+//       name: 'Lua',
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-lua.svg',
+//     },
+//     {
+//       name: 'C++',
+//       target: ['APP'],
+//       icon: '',
+//     },
+//     {
+//       name: 'Swift',
+//       target: ['IOS', 'APP'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-swift.svg',
+//     },
+//     {
+//       name: 'Objective-C',
+//       target: ['IOS', 'APP'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-objectivec.svg',
+//     },
+//     {
+//       name: 'PHP',
+//       target: ['后端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-php.svg',
+//     },
+//   ],
+//   // 框架
+//   frame: [
+//     {
+//       name: 'Vue',
+//       target: ['前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-vue.svg',
+//     },
+//     {
+//       name: 'Nuxt',
+//       target: ['前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-nuxt.svg',
+//     },
+//     {
+//       name: 'Electron',
+//       target: ['前端', 'APP'],
+//       icon: 'https://api.iconify.design/vscode-icons:folder-type-electron.svg',
+//     },
+//     {
+//       name: 'Tauri',
+//       target: ['前端', 'APP'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-tauri.svg',
+//     },
+//     {
+//       name: 'Vite',
+//       target: ['前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-vite.svg',
+//     },
+//     {
+//       name: 'Unocss',
+//       target: ['前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-unocss.svg',
+//     },
+//     {
+//       name: 'Webpack',
+//       target: ['前端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-webpack.svg',
+//     },
+//     {
+//       name: 'Nestjs',
+//       target: ['后端'],
+//       icon: 'https://api.iconify.design/vscode-icons:file-type-nestjs.svg',
+//     },
+//   ],
+// })
 
-const option = ref<EChartsOption>({
-  tooltip: {},
-  animationDurationUpdate: 1500,
-  animationEasingUpdate: 'quinticInOut',
-  label: {
-    show: true,
-    fontSize: 12,
-  },
-  legend: {
-    // x: 'center',
-    show: false,
-    data: ['夫妻', '战友', '亲戚'],
-  },
-  series: [
-    {
-      type: 'graph',
-      layout: 'force',
-      symbolSize: 45,
-      // adjacency: true,
-      roam: true,
-      categories: [
-        {
-          name: '关系测试',
-          itemStyle: {
-            color: '#009800',
-          },
-        },
-        {
-          name: '语言',
-          itemStyle: {
-            color: '#4592FF',
-          },
-        },
-      ],
-      label: {
-        show: true,
-      },
-      force: {
-        repulsion: 1000,
-      },
-      edgeSymbolSize: [4, 50],
-      edgeLabel: {
-        show: true,
-        fontSize: 10,
-        formatter: '{c}',
-      },
-      data: [
-        {
-          name: '前端',
-          category: 0,
-          draggable: true,
-        },
-        {
-          name: 'vue',
-          category: 0,
-          draggable: true,
-        },
-        {
-          name: 'nuxt',
-          category: 0,
-          draggable: true,
-        },
-        {
-          name: 'js',
-          category: 1,
-          draggable: true,
-        },
-        {
-          name: 'ts',
-          category: 1,
-          draggable: true,
-        },
-      ],
-      links: [
-        {
-          source: 'vue',
-          target: '前端',
-          value: '',
-        },
-        {
-          source: 'nuxt',
-          target: '前端',
-          value: '子女',
-        },
-        {
-          source: 'nuxt',
-          target: 'vue',
-          value: '夫妻',
-        },
-      ],
-      lineStyle: {
-        opacity: 0.9,
-        width: 1,
-        curveness: 0,
-      },
-    },
-  ],
-})
+// const option = ref<EChartsOption>({
+//   tooltip: {},
+//   animationDurationUpdate: 1500,
+//   animationEasingUpdate: 'quinticInOut',
+//   label: {
+//     show: true,
+//     fontSize: 12,
+//   },
+//   legend: {
+//     // x: 'center',
+//     show: false,
+//     data: ['夫妻', '战友', '亲戚'],
+//   },
+//   series: [
+//     {
+//       type: 'graph',
+//       layout: 'force',
+//       symbolSize: 45,
+//       // adjacency: true,
+//       roam: true,
+//       categories: [
+//         {
+//           name: '关系测试',
+//           itemStyle: {
+//             color: '#009800',
+//           },
+//         },
+//         {
+//           name: '语言',
+//           itemStyle: {
+//             color: '#4592FF',
+//           },
+//         },
+//       ],
+//       label: {
+//         show: true,
+//       },
+//       force: {
+//         repulsion: 1000,
+//       },
+//       edgeSymbolSize: [4, 50],
+//       edgeLabel: {
+//         show: true,
+//         fontSize: 10,
+//         formatter: '{c}',
+//       },
+//       data: [
+//         {
+//           name: '前端',
+//           category: 0,
+//           draggable: true,
+//         },
+//         {
+//           name: 'vue',
+//           category: 0,
+//           draggable: true,
+//         },
+//         {
+//           name: 'nuxt',
+//           category: 0,
+//           draggable: true,
+//         },
+//         {
+//           name: 'js',
+//           category: 1,
+//           draggable: true,
+//         },
+//         {
+//           name: 'ts',
+//           category: 1,
+//           draggable: true,
+//         },
+//       ],
+//       links: [
+//         {
+//           source: 'vue',
+//           target: '前端',
+//           value: '',
+//         },
+//         {
+//           source: 'nuxt',
+//           target: '前端',
+//           value: '子女',
+//         },
+//         {
+//           source: 'nuxt',
+//           target: 'vue',
+//           value: '夫妻',
+//         },
+//       ],
+//       lineStyle: {
+//         opacity: 0.9,
+//         width: 1,
+//         curveness: 0,
+//       },
+//     },
+//   ],
+// })
 
-// 数据处理-data
-const baseDataList = ref<any[]>([])
-const dataList = ref<any[]>([])
-function handleData() {
-  const tempList = []
-  Object.keys(tempData.value).forEach((key) => {
-    tempData.value[key].forEach((item: any) => {
-      tempList.push({
-        name: item.name,
-        draggable: true,
-        symbol: item.icon ? `image://${item.icon}` : 'circle',
-        label: {
-          position: 'bottom',
-        },
-      })
-      baseDataList.value.push(item)
-    })
-  })
-  dataList.value = tempList
-  option.value.series[0].data = tempList
-}
-// 数据处理-links
-const linksList = ref<any[]>([])
-function handleLinks() {
-  const tempLinksList = []
+// // 数据处理-data
+// const baseDataList = ref<any[]>([])
+// const dataList = ref<any[]>([])
+// function handleData() {
+//   const tempList = []
+//   Object.keys(tempData.value).forEach((key) => {
+//     tempData.value[key].forEach((item: any) => {
+//       tempList.push({
+//         name: item.name,
+//         draggable: true,
+//         symbol: item.icon ? `image://${item.icon}` : 'circle',
+//         label: {
+//           position: 'bottom',
+//         },
+//       })
+//       baseDataList.value.push(item)
+//     })
+//   })
+//   dataList.value = tempList
+//   option.value.series[0].data = tempList
+// }
+// // 数据处理-links
+// const linksList = ref<any[]>([])
+// function handleLinks() {
+//   const tempLinksList = []
 
-  // 循环，判断如果有target
-  baseDataList.value.forEach((item: any) => {
-    if (item.target) {
-      item.target.forEach((target: any) => {
-        tempLinksList.push({
-          source: item.name,
-          target,
-          value: '',
-        })
-      })
-    }
-  })
+//   // 循环，判断如果有target
+//   baseDataList.value.forEach((item: any) => {
+//     if (item.target) {
+//       item.target.forEach((target: any) => {
+//         tempLinksList.push({
+//           source: item.name,
+//           target,
+//           value: '',
+//         })
+//       })
+//     }
+//   })
 
-  linksList.value = tempLinksList
-  option.value.series[0].links = linksList.value
-}
-onMounted(() => {
-  handleData()
-  handleLinks()
-})
+//   linksList.value = tempLinksList
+//   option.value.series[0].links = linksList.value
+// }
+// onMounted(() => {
+//   handleData()
+//   handleLinks()
+// })
 </script>
 
 <template>
   <div>
+    1
     <!-- @vue-ignore -->
-    <VChart class="chart" :option="option" autoresize />
+    <!-- <VChart class="chart" :option="option" autoresize /> -->
   </div>
 </template>
 
