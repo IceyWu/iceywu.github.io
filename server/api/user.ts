c6604bc287d98a7c90b0384e7d54caf83509295a
IceyWu <3128006406@qq.com>
2025-01-01 16:41:35 +0800

    fix: update api

--- server/api/user.ts
+++ server/api/user.ts
@@ -1,6 +1,7 @@
 import { useOctokit } from '../utils/github'
 
 export default defineEventHandler(async () => {
-  const { data } = await useOctokit().request('GET /user')
-  return data
+  // const { data } = await useOctokit().request('GET /user')
+  // return data
+  return $fetch<User>('https://api.github.com/users/iceywu')
 })
