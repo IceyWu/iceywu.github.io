3ceda6cf8019d756d6a23e14ecd4f149928fd331
IceyWu <3128006406@qq.com>
2025-01-02 11:03:55 +0800

    fix: 使用 Octokit 更新贡献和仓库 API 请求逻辑

--- server/api/contribution.ts
+++ server/api/contribution.ts
@@ -1,7 +1,23 @@
-// import type { ContributeData } from '~/types'
+import { useOctokit } from '../utils/github'
 
 export default defineEventHandler(async (event) => {
   const { name, year } = getQuery(event)
-  const API = `https://github-contributions-api.jogruber.de/v4/${name}?y=${year}`
-  return $fetch<ContributeData>(API)
+  const octokit = useOctokit()
+  try {
+    const response = await octokit.request('GET /users/{username}/events', {
+      username: name as string,
+      per_page: 100,
+    })
+
+    const contributions = response.data.filter((d) => {
+      const eventYear = new Date(d.created_at!).getFullYear()
+      return eventYear === Number.parseInt(year as string, 10)
+    })
+
+    return contributions
+  }
+  catch (error) {
+    console.error('Error fetching contributions:', error)
+    throw error
+  }
 })
