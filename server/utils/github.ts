3a839183f67ed1c0c9dd0c24006769778382c1d9
IceyWu <3128006406@qq.com>
2025-03-03 16:07:17 +0800

    chore: update deps

--- server/utils/github.ts
+++ server/utils/github.ts
@@ -4,7 +4,7 @@ let _octokit: Octokit
 
 export function useOctokit() {
   // eslint-disable-next-line no-console
-  // console.log('🌳-----process.env-----', process.env)
+  console.log('🌳-----process.env-----', process.env)
   if (!_octokit) {
     _octokit = new Octokit({
       auth: process.env.MY_TOKEN,
